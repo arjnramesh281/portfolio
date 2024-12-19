23be17f94ea76271a27f0c4f4cbca615df47c83d
arjnramesh281 <arjunramesh281@gmail.com>
2024-12-19 11:31:58 +0530

    portfolio

---
+++ home.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template,request,redirect,url_for
+import sqlite3
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+app.run()
